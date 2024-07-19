7d36bb98a186591c6d6766113cc849cccb0fb971
ayushxpal <ayush.pal23@lpu.in>
2024-07-19 15:10:39 +0530

    changes are constant

--- 19jul.cpp
+++ 19jul.cpp
@@ -1,16 +1,34 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int i, max=0;
+//     int arr[]={12,123,22,3};
+    
+//     for(i=0;i<4;i++)
+//     {
+//         if(arr[i]>max)
+//         max=arr[i];
+//         // cout<<max<<endl;
+//     }
+
+//         cout<<max<<endl;
+
+// }
+
 #include <iostream>
 using namespace std;
 int main(){
-    int i, max=0;
-    int arr[]={12,123,22,3};
-    
-    for(i=0;i<4;i++)
+    int number;
+    cout<<"enter a number:";
+    cin>>number;
+    int sum=0;
+    while (number!=0)
     {
-        if(arr[i]>max)
-        max=arr[i];
-        // cout<<max<<endl;
+        int digit=number%10;
+        number=number/10;
+        sum+=digit;
     }
 
-        cout<<max<<endl;
-
+    cout<<"sum of digits:"<<sum<<endl;
+    return 0;
 }
