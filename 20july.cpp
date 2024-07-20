8ea3ff7a0075d03eebe212938b9826e493356bbe
ayushxpal <ayush.pal23@lpu.in>
2024-07-20 21:07:37 +0530

    maximum number from an array

--- 20july.cpp
+++ 20july.cpp
@@ -1,17 +1,36 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int number;
+//     cout<<"enter a number: ";
+//     cin>>number;
+//     int sum=0;
+//     while(number!=0)
+//     {
+//         int digit=number%10;
+//         number=number/10;
+//         sum+=digit;
+//     }
+//     cout<<"sum of digit(s): "<<sum<<endl;
+
+
+// }
+
+
+
 #include <iostream>
 using namespace std;
 int main(){
-    int number;
-    cout<<"enter a number: ";
-    cin>>number;
-    int sum=0;
-    while(number!=0)
+    int arr[]={123,342,45,222};
+    int i, max=0;
+    for(i=0;i<4;i++)
     {
-        int digit=number%10;
-        number=number/10;
-        sum+=digit;
+        if(arr[i]>max)
+        {
+            max=arr[i];
+            
+        }
+        
     }
-    cout<<"sum of digit(s): "<<sum<<endl;
-
-
+    cout<<max<<endl;
 }
