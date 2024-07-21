5c2ceb807bbf8e9ac47d5cde78d2b25d3b162624
ayushxpal <ayush.pal23@lpu.in>
2024-07-21 21:35:37 +0530

    21july

---
+++ 21jul.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int number;
+    cout<<"enter a number: ";
+    cin>>number;
+    int sum=0;
+    while(number!=0)
+    {
+        int digit=number%10;
+        number=number/10;
+        sum+=digit;
+    }
+    cout<<"sum of digit(s): "<<sum<<endl;   
+    return 0;
+}
