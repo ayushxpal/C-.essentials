d246959719a460081c1319f570afc3a9d6ba1701
ayushxpal <ayush.pal23@lpu.in>
2024-07-21 21:54:10 +0530

    swap numbers without variable

--- 21jul.cpp
+++ 21jul.cpp
@@ -1,16 +1,30 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int number;
+//     cout<<"enter a number: ";
+//     cin>>number;
+//     int sum=0;
+//     while(number!=0)
+//     {
+//         int digit=number%10;
+//         number=number/10;
+//         sum+=digit;
+//     }
+//     cout<<"sum of digit(s): "<<sum<<endl;   
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
 int main(){
-    int number;
-    cout<<"enter a number: ";
-    cin>>number;
-    int sum=0;
-    while(number!=0)
-    {
-        int digit=number%10;
-        number=number/10;
-        sum+=digit;
-    }
-    cout<<"sum of digit(s): "<<sum<<endl;   
+    int number1;
+    int number2;
+    cout<<"enter the numbers: ";
+    cin>>number1>>number2;
+    number1=number1+number2;
+    number2=number1-number2;
+    number1=number1-number2;
+    cout<<"numbers after swapping: "<<number1<<" "<<number2<<endl;
     return 0;
 }
