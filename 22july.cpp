cf10535ec5227eb263321809f4bad52a4d725b51
ayushxpal <ayush.pal23@lpu.in>
2024-07-22 14:47:41 +0530

    prime number check

--- 22july.cpp
+++ 22july.cpp
@@ -1,77 +1,109 @@
+// // // #include <iostream>
+// // // using namespace std;
+// // // int main(){
+// // //     int number;
+// // //     int d=2;
+// // //     cout<<"enter a number:";
+// // //     cin>>number;
+// // //     bool prime=true;
+// // //     while(d!=number)
+// // //     {
+// // //         if(number%d==0)
+// // //         {
+// // //             prime=false;
+// // //             break;
+// // //         }
+
+// // //         d++;
+// // //     }
+
+// // //     if (prime)
+// // //     {
+// // //         cout<<"prime number";
+// // //     }
+// // //     else{
+// // //         cout<<"not a prime number";
+// // //     }
+// // // }
+
 // // #include <iostream>
 // // using namespace std;
 // // int main(){
 // //     int number;
 // //     int d=2;
-// //     cout<<"enter a number:";
+// //     cout<<"enter a number: ";
 // //     cin>>number;
-// //     bool prime=true;
+// //     bool isPrime=true;
 // //     while(d!=number)
 // //     {
 // //         if(number%d==0)
 // //         {
-// //             prime=false;
+// //             isPrime=false;
 // //             break;
 // //         }
 
 // //         d++;
 // //     }
 
-// //     if (prime)
+// //     if(isPrime)
 // //     {
 // //         cout<<"prime number";
 // //     }
-// //     else{
+// //     else
+// //     {
 // //         cout<<"not a prime number";
 // //     }
+
+// //     return 0;
 // // }
+
 
 // #include <iostream>
 // using namespace std;
 // int main(){
-//     int number;
-//     int d=2;
-//     cout<<"enter a number: ";
-//     cin>>number;
-//     bool isPrime=true;
-//     while(d!=number)
+//     int arr[]={122,123,4,4,123};
+//     int i, max=0;
+//     for(i=0;i<5;i++)
 //     {
-//         if(number%d==0)
+//         if(arr[i]>max)
 //         {
-//             isPrime=false;
-//             break;
+//             max=arr[i];
+//             // cout<<"greatest number from the array is:"<<max<<endl;
 //         }
-
-//         d++;
+//         // cout<<"greatest number from the array is:"<<max<<endl;
 //     }
 
-//     if(isPrime)
-//     {
-//         cout<<"prime number";
-//     }
-//     else
-//     {
-//         cout<<"not a prime number";
-//     }
-
-//     return 0;
+//     cout<<"greatest number from the array is:"<<max<<endl;
 // }
-
 
 #include <iostream>
 using namespace std;
 int main(){
-    int arr[]={122,123,4,4,123};
-    int i, max=0;
-    for(i=0;i<5;i++)
+    int number;
+    int d=2;
+    cout<<"enter any number: ";
+    cin>>number;
+    bool isPrime=true;
+    while(d!=number)
     {
-        if(arr[i]>max)
+        if(number%d==0)
         {
-            max=arr[i];
-            // cout<<"greatest number from the array is:"<<max<<endl;
+            isPrime=false;
+            break;
         }
-        // cout<<"greatest number from the array is:"<<max<<endl;
+
+        d++;
     }
 
-    cout<<"greatest number from the array is:"<<max<<endl;
+    if(isPrime)
+    {
+        cout<<"prime number";
+    }
+
+    else
+    {
+        cout<<"not a prime number";
+    }
+
+    return 0;
 }
