0fe8d2494c4db39a8e937baa26177732ed1026ad
ayushxpal <ayush.pal23@lpu.in>
2024-07-23 09:54:37 +0530

    prime number check

---
+++ 23jul.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int number;
+    int d=2;
+    cout<<"enter any number: ";
+    cin>>number;
+    bool isPrime=true;
+    while(d!=number)
+    {
+        if(number%d==0)
+        {
+            isPrime=false;
+            break;
+        }
+        d++;
+
+    }
+
+    if(isPrime)
+    {
+        cout<<"a prime number";
+    }
+    else
+    {
+        cout<<"not a prime number";
+    }
+}
