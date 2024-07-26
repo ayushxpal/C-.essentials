1e3a814a1702127098f8a06cc7a12387b0f0f1a2
ayushxpal <ayush.pal23@lpu.in>
2024-07-26 13:50:46 +0530

    reverse of any number

--- 26july.cpp
+++ 26july.cpp
@@ -1,31 +1,70 @@
+// // #include <iostream>
+// // using namespace std;
+// // int main()
+// // {
+// //     int number;
+// //     int d=2;
+// //     bool isPrime=true;
+// //     cout<<"enter a number: ";
+// //     cin>>number;
+// //     while(d!=number)
+// //     {
+// //         if(number%d==0)
+// //         isPrime=false;
+// //         break;
+// //     }
+
+// //     d++;
+
+// //     if(isPrime)
+// //     {
+// //         cout<<number<<" is a prime number";
+// //     }
+
+// //     else
+// //     {
+// //         cout<<number<<" is not a prime number";
+// //     }
+
+// //     return 0;
+
+// // }
+
+
+// #include <iostream>  // This line is missing in your code, it's necessary for cout to work
+// using namespace std;
+
+// int main() {
+//     int a = 1223;  // The number to be reversed
+//     int res = 0;   // The variable to store the reversed number
+
+//     while(a != 0) {  // Loop until all digits of 'a' are processed
+//         int dig = a % 10;  // Get the last digit of 'a' (1223 % 10 = 3)
+//         res = res * 10 + dig;  // Add the digit to 'res' (res = 0 * 10 + 3 = 3)
+//         a = a / 10;  // Remove the last digit from 'a' (1223 / 10 = 122)
+//     }
+
+//     cout << res;  // Print the reversed number (3231)
+//     return 0;
+// }
+
+
 #include <iostream>
 using namespace std;
 int main()
 {
     int number;
-    int d=2;
-    bool isPrime=true;
+    int rev_number=0;
     cout<<"enter a number: ";
     cin>>number;
-    while(d!=number)
+    int original_number=number;
+    while(number!=0)
     {
-        if(number%d==0)
-        isPrime=false;
-        break;
+        int dig=number%10;
+        rev_number=rev_number*10+dig;
+        number=number/10;
     }
 
-    d++;
-
-    if(isPrime)
-    {
-        cout<<number<<" is a prime number";
-    }
-
-    else
-    {
-        cout<<number<<" is not a prime number";
-    }
-
+    cout<<rev_number<<" is reverse of "<<original_number;
     return 0;
-
 }
