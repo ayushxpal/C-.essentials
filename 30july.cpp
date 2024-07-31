5f704edf966dfc97d5e082fa91229cb13eae8bad
ayushxpal <ayush.pal23@lpu.in>
2024-07-31 22:06:15 +0530

    factorial of number

--- 30july.cpp
+++ 30july.cpp
@@ -1,19 +1,46 @@
+// // #include <iostream>
+// // using namespace std;
+// // int factorial(int n)
+// // {
+// //     int res=1;
+// //     for(int i=2;i<=n;i++)
+// //     {
+// //         res=res*i;
+// //     }
+// //     return res;
+// // }
+// // int main()
+// // {
+// //     int n;
+// //     cout<<"Enter a number: ";
+// //     cin>>n;
+// //     cout<<"Factorial of "<<n<<" is "<<factorial(n);
+// //     return 0;
+// // }
+
+
+// #include <iostream>
+// using namespace std;
+// int factorial (int n)
+// {
+//     int res=1;
+//     for(int i=2;i<=n;i++)
+//     {
+//         res=res*i;
+//     }  
+
+//     return res;
+
+// }
+
+// int main()
+// {
+//     int n;
+//     cout<<"Enter a number: ";
+//     cin>>n;
+//     cout<<"Factorial of "<<n<<" is "<<factorial(n);
+//     return 0;
+// }
+
+
 #include <iostream>
-using namespace std;
-int factorial(int n)
-{
-    int res=1;
-    for(int i=2;i<=n;i++)
-    {
-        res=res*i;
-    }
-    return res;
-}
-int main()
-{
-    int n;
-    cout<<"Enter a number: ";
-    cin>>n;
-    cout<<"Factorial of "<<n<<" is "<<factorial(n);
-    return 0;
-}
