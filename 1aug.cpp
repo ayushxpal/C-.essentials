01eca5da95d732d04ad86487ecd9cb740cdfdd0f
ayushxpal <ayush.pal23@lpu.in>
2024-08-01 11:32:05 +0530

    check if a number is prime or not

--- 1aug.cpp
+++ 1aug.cpp
@@ -1,27 +1,46 @@
+// // #include <iostream>
+// // using namespace std;
+// // int factorial(int n)
+// // {
+// //     int res=1;
+// //     int i;
+// //     for(i=1;i<=n;i++)
+// //     {
+// //         res=res*i;
+// //     }
+
+// //     return res;
+
+// // }
+
+// // int main()
+// // {
+// //     int n;
+// //     cout<<"Enter the number: ";
+// //     cin>>n;
+// //     cout<<"Factorial of "<<n<<" is: "<<factorial(n);
+// //     return 0;
+// // }
+
+
+
 // #include <iostream>
 // using namespace std;
-// int factorial(int n)
-// {
-//     int res=1;
-//     int i;
-//     for(i=1;i<=n;i++)
-//     {
-//         res=res*i;
-//     }
-
-//     return res;
-
-// }
-
 // int main()
 // {
-//     int n;
-//     cout<<"Enter the number: ";
-//     cin>>n;
-//     cout<<"Factorial of "<<n<<" is: "<<factorial(n);
-//     return 0;
+//     int num;
+//     cout << "Enter a number: ";
+//     cin >> num;
+//     int og_num=num;
+//     int sum=0;
+//     while(num!=0)
+//     {
+//         int digit=num%10;
+//         num=num/10;
+//         sum+=digit;
+//     }
+//     cout<<"sum of digits of "<<og_num<<" is "<<sum;
 // }
-
 
 
 #include <iostream>
@@ -29,15 +48,29 @@ using namespace std;
 int main()
 {
     int num;
-    cout << "Enter a number: ";
-    cin >> num;
-    int og_num=num;
-    int sum=0;
-    while(num!=0)
+    int d=2;
+    cout<<"enter a number: ";
+    cin>>num;
+    bool isPrime=true;
+    while(d!=num)
     {
-        int digit=num%10;
-        num=num/10;
-        sum+=digit;
+        if(num%d==0)
+        {
+            isPrime=false;
+            break;
+        }
+        d++;
     }
-    cout<<"sum of digits of "<<og_num<<" is "<<sum;
+
+    if(isPrime)
+    {
+        cout<<"PRIME NUMBER it is";
+    }
+
+    else
+    {
+        cout<<"NOT A PRIME NUMBER";
+    }
+
+    return 0;
 }
