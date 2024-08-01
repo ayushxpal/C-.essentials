f1f583c6453764c12160c5dc90f22f31a2a7b5e2
ayushxpal <ayush.pal23@lpu.in>
2024-08-01 11:10:35 +0530

    sum of digits

--- 1aug.cpp
+++ 1aug.cpp
@@ -1,23 +1,43 @@
+// #include <iostream>
+// using namespace std;
+// int factorial(int n)
+// {
+//     int res=1;
+//     int i;
+//     for(i=1;i<=n;i++)
+//     {
+//         res=res*i;
+//     }
+
+//     return res;
+
+// }
+
+// int main()
+// {
+//     int n;
+//     cout<<"Enter the number: ";
+//     cin>>n;
+//     cout<<"Factorial of "<<n<<" is: "<<factorial(n);
+//     return 0;
+// }
+
+
+
 #include <iostream>
 using namespace std;
-int factorial(int n)
-{
-    int res=1;
-    int i;
-    for(i=1;i<=n;i++)
-    {
-        res=res*i;
-    }
-
-    return res;
-
-}
-
 int main()
 {
-    int n;
-    cout<<"Enter the number: ";
-    cin>>n;
-    cout<<"Factorial of "<<n<<" is: "<<factorial(n);
-    return 0;
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    int og_num=num;
+    int sum=0;
+    while(num!=0)
+    {
+        int digit=num%10;
+        num=num/10;
+        sum+=digit;
+    }
+    cout<<"sum of digits of "<<og_num<<" is "<<sum;
 }
