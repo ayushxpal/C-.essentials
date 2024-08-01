b08a5fd9e04fb47243a647d0f8e7a9ddde8b8624
ayushxpal <ayush.pal23@lpu.in>
2024-08-01 11:59:45 +0530

    armstrong number

--- 1aug.cpp
+++ 1aug.cpp
@@ -1,27 +1,46 @@
+// // // #include <iostream>
+// // // using namespace std;
+// // // int factorial(int n)
+// // // {
+// // //     int res=1;
+// // //     int i;
+// // //     for(i=1;i<=n;i++)
+// // //     {
+// // //         res=res*i;
+// // //     }
+
+// // //     return res;
+
+// // // }
+
+// // // int main()
+// // // {
+// // //     int n;
+// // //     cout<<"Enter the number: ";
+// // //     cin>>n;
+// // //     cout<<"Factorial of "<<n<<" is: "<<factorial(n);
+// // //     return 0;
+// // // }
+
+
+
 // // #include <iostream>
 // // using namespace std;
-// // int factorial(int n)
-// // {
-// //     int res=1;
-// //     int i;
-// //     for(i=1;i<=n;i++)
-// //     {
-// //         res=res*i;
-// //     }
-
-// //     return res;
-
-// // }
-
 // // int main()
 // // {
-// //     int n;
-// //     cout<<"Enter the number: ";
-// //     cin>>n;
-// //     cout<<"Factorial of "<<n<<" is: "<<factorial(n);
-// //     return 0;
+// //     int num;
+// //     cout << "Enter a number: ";
+// //     cin >> num;
+// //     int og_num=num;
+// //     int sum=0;
+// //     while(num!=0)
+// //     {
+// //         int digit=num%10;
+// //         num=num/10;
+// //         sum+=digit;
+// //     }
+// //     cout<<"sum of digits of "<<og_num<<" is "<<sum;
 // // }
-
 
 
 // #include <iostream>
@@ -29,48 +48,68 @@
 // int main()
 // {
 //     int num;
-//     cout << "Enter a number: ";
-//     cin >> num;
-//     int og_num=num;
-//     int sum=0;
-//     while(num!=0)
+//     int d=2;
+//     cout<<"enter a number: ";
+//     cin>>num;
+//     bool isPrime=true;
+//     while(d!=num)
 //     {
-//         int digit=num%10;
-//         num=num/10;
-//         sum+=digit;
+//         if(num%d==0)
+//         {
+//             isPrime=false;
+//             break;
+//         }
+//         d++;
 //     }
-//     cout<<"sum of digits of "<<og_num<<" is "<<sum;
-// }
 
+//     if(isPrime)
+//     {
+//         cout<<"PRIME NUMBER it is";
+//     }
+
+//     else
+//     {
+//         cout<<"NOT A PRIME NUMBER";
+//     }
+
+//     return 0;
+// }
 
 #include <iostream>
 using namespace std;
+int sumofcubes(int n)
+{
+    int res=0;
+    while(n!=0)
+    {
+        int digit=n%10;
+        res=res+(digit*digit*digit);
+        n=n/10;
+
+    }
+
+    return res;
+
+}
+
 int main()
 {
     int num;
-    int d=2;
-    cout<<"enter a number: ";
+    cout<<"Enter a number: ";
     cin>>num;
-    bool isPrime=true;
-    while(d!=num)
+    if(num==sumofcubes(num))
     {
-        if(num%d==0)
-        {
-            isPrime=false;
-            break;
-        }
-        d++;
-    }
-
-    if(isPrime)
-    {
-        cout<<"PRIME NUMBER it is";
+        cout<<"Armstrong number";
     }
 
     else
     {
-        cout<<"NOT A PRIME NUMBER";
+        cout<<"Not an Armstrong number";
     }
-
     return 0;
 }
+
+
+
+
+
