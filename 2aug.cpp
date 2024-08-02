16bd2f57ece0e0253d494556180ed1edf7af3c4c
ayushxpal <ayush.pal23@lpu.in>
2024-08-02 23:02:56 +0530

    armstrong

--- 2aug.cpp
+++ 2aug.cpp
@@ -1,3 +1,63 @@
+// // // // #include <iostream>
+// // // // using namespace std;
+// // // // int sumofcubes(int n)
+// // // // {
+// // // //     int res=0;
+// // // //     while(n!=0)
+// // // //     {
+// // // //         int digit=n%10;
+// // // //         res=res+(digit*digit*digit);
+// // // //         n=n/10;
+// // // //     }
+// // // //     return res;
+// // // // }
+// // // //     // return res;
+
+// // // //     int main()
+// // // //     {
+// // // //         int n;
+// // // //         cout<<"enter a number: ";
+// // // //         cin>>n;
+// // // //         if(n==sumofcubes(n))
+// // // //         {
+// // // //             cout<<"armstrong";
+// // // //         }
+// // // //         else{
+// // // //             cout<<"not an armstrong (u jerkk)";
+// // // //         }
+// // // //         return 0;
+// // // //     }
+
+
+// // // #include <iostream>
+// // // using namespace std;
+// // // int sumofcubes(int n)
+// // // {
+// // //     int res=0;
+// // //     while(n!=0)
+// // //     {
+// // //         int digit=n%10;
+// // //         res=res+(digit*digit*digit);
+// // //         n=n/10;
+// // //     }
+// // //     return res;
+
+// // // }
+
+// // // int main()
+// // // {
+// // //     int n;
+// // //     cout<<"enter any number: ";
+// // //     cin>>n;
+// // //     if(n==sumofcubes(n))
+// // //     {
+// // //         cout<<"arm is strong";
+// // //     }
+// // //     else{
+// // //         cout<<"arm is weak";
+// // //     }
+// // // }
+
 // // #include <iostream>
 // // using namespace std;
 // // int sumofcubes(int n)
@@ -8,86 +68,74 @@
 // //         int digit=n%10;
 // //         res=res+(digit*digit*digit);
 // //         n=n/10;
+        
+        
 // //     }
 // //     return res;
-// // }
-// //     // return res;
 
-// //     int main()
+// // }
+
+// // int main()
+// // {
+// //     int n;
+// //     cout<<"enter a number: ";
+// //     cin>>n;
+// //     if(n==sumofcubes(n))
 // //     {
-// //         int n;
-// //         cout<<"enter a number: ";
-// //         cin>>n;
-// //         if(n==sumofcubes(n))
-// //         {
-// //             cout<<"armstrong";
-// //         }
-// //         else{
-// //             cout<<"not an armstrong (u jerkk)";
-// //         }
-// //         return 0;
+// //         cout<<"armstrong";
 // //     }
+// //     else
+// //     {
+// //         cout<<"not an armstrong";
+// //     }
+
+// //     return 0;
+
+// // }
+
 
 
 // #include <iostream>
 // using namespace std;
-// int sumofcubes(int n)
-// {
-//     int res=0;
-//     while(n!=0)
-//     {
-//         int digit=n%10;
-//         res=res+(digit*digit*digit);
-//         n=n/10;
-//     }
-//     return res;
-
-// }
-
 // int main()
 // {
-//     int n;
+//     int a[10], n, i;
 //     cout<<"enter any number: ";
 //     cin>>n;
-//     if(n==sumofcubes(n))
+//     for(i=0; n>0; i++)
 //     {
-//         cout<<"arm is strong";
+//         a[i]=n%2;
+//         n=n/2;
 //     }
-//     else{
-//         cout<<"arm is weak";
+
+//     cout<<"binary of the given number is: ";
+//     for(i=i-1; i>=0; i--)
+//     {
+//         cout<<a[i];
 //     }
+
+//     return 0;
 // }
+
 
 #include <iostream>
 using namespace std;
-int sumofcubes(int n)
-{
-    int res=0;
-    while(n!=0)
-    {
-        int digit=n%10;
-        res=res+(digit*digit*digit);
-        n=n/10;
-        
-        
-    }
-    return res;
-
-}
-
 int main()
 {
-    int n;
-    cout<<"enter a number: ";
+    int a[10], n, i;
+    cout<<"enter any number: ";
     cin>>n;
-    if(n==sumofcubes(n))
+    for(i=0; n>0; i++)
     {
-        cout<<"armstrong";
+        a[i]=n%2;
+        n=n/2;
     }
-    else
+
+    cout<<"binary of the given number is: ";
+    for(i=i-1; i>=0; i--)
     {
-        cout<<"not an armstrong";
-    }
+        cout<<a[i];
+    }       
 
     return 0;
 
