072682b1385ce43714d9af297d81e8b25b003477
ayushxpal <ayush.pal23@lpu.in>
2024-08-03 21:45:14 +0530

    binary

---
+++ 3aug.cpp
@@ -0,0 +1,42 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a[10], n, i;
+//     cout<<"enter any number: ";
+//     cin>>n;
+//     for(i=0;n>0;i++)
+//     {
+//         a[i]=n%2;
+//         n=n/2;
+//     }
+
+//     cout<<"binary of the given number is: ";
+//     for(i=i-1;i>=0;i--)
+//     {
+//         cout<<a[i];
+//     }
+//     return 0;
+
+// }
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a[10], n, i;
+    cout<<"enter a number: ";
+    cin>>n;
+    for (i=0;0>n;i++)
+    {
+        a[i]=n/2;
+        n=n/2;
+    }
+
+    cout<<"binary of the number is: ";
+    for(i=i-1;i>=0;i++)
+    {
+        cout<<a[i];
+    }
+    return 0;
+}
