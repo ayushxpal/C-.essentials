a2bae5c1f5e7c769713a14088f4b472ce12a4190
ayushxpal <ayush.pal23@lpu.in>
2024-08-04 13:06:08 +0530

    decimal to binary

---
+++ binarytodecimal.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int convertBinarytoDecimal(int n) 
+{
+    int decimalNumber=0, i=0, dig;
+    while(n!=0)
+    {
+        dig=n%10;
+        n=n/10;
+        decimalNumber+=dig*pow(2,i);
+        ++i;
+    }
+    return decimalNumber;
+}
+
+int main()
+{
+    int n;
+    cout<<"enter a number: ";
+    cin>>n;
+    cout<<"Decimal of binary "<<n<<" is "<<convertBinarytoDecimal(n);
+    return 0;
+}
