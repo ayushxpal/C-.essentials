7c8a2643943835cc462ef21a2526ff4bcabaa05c
ayushxpal <ayush.pal23@lpu.in>
2024-08-04 13:29:22 +0530

    binarytodecimal

--- 3aug.cpp
+++ 3aug.cpp
@@ -1,42 +1,47 @@
+// // // #include <iostream>
+// // // using namespace std;
+// // // int main()
+// // // {
+// // //     int a[10], n, i;
+// // //     cout<<"enter any number: ";
+// // //     cin>>n;
+// // //     for(i=0;n>0;i++)
+// // //     {
+// // //         a[i]=n%2;
+// // //         n=n/2;
+// // //     }
+
+// // //     cout<<"binary of the given number is: ";
+// // //     for(i=i-1;i>=0;i--)
+// // //     {
+// // //         cout<<a[i];
+// // //     }
+// // //     return 0;
+
+// // // }
+
+// // #include <iostream>
+// // using namespace std;
+// // int main()
+// // {
+// //     int a[10], n, i;
+// //     cout<<"enter a number: ";
+// //     cin>>n;
+// //     for (i=0;0>n;i++)
+// //     {
+// //         a[i]=n/2;
+// //         n=n/2;
+// //     }
+
+// //     cout<<"binary of the number is: ";
+// //     for(i=i-1;i>=0;i++)
+// //     {
+// //         cout<<a[i];
+// //     }
+// //     return 0;
+// // }
+
 // #include <iostream>
+// #include <cmath>
 // using namespace std;
-// int main()
-// {
-//     int a[10], n, i;
-//     cout<<"enter any number: ";
-//     cin>>n;
-//     for(i=0;n>0;i++)
-//     {
-//         a[i]=n%2;
-//         n=n/2;
-//     }
-
-//     cout<<"binary of the given number is: ";
-//     for(i=i-1;i>=0;i--)
-//     {
-//         cout<<a[i];
-//     }
-//     return 0;
-
-// }
-
-#include <iostream>
-using namespace std;
-int main()
-{
-    int a[10], n, i;
-    cout<<"enter a number: ";
-    cin>>n;
-    for (i=0;0>n;i++)
-    {
-        a[i]=n/2;
-        n=n/2;
-    }
-
-    cout<<"binary of the number is: ";
-    for(i=i-1;i>=0;i++)
-    {
-        cout<<a[i];
-    }
-    return 0;
-}
+// int 
--- 4aug.cpp
+++ 4aug.cpp
@@ -1,18 +1,44 @@
+// #include <iostream>
+// #include <cmath>
+// using namespace std;
+// int convertBinarytoDecimal(int n) {
+//     int decimalNumber=0, i=0, dig;
+//     while(n!=0)
+//     {
+//         dig=n%10;
+//         n=n/10;
+//         decimalNumber+=dig*pow(2,i);
+//         ++i;
+//     }
+
+//     return decimalNumber;
+
+// }
+
+// int main()
+// {
+//     int n;
+//     cout<<"enter a number: ";
+//     cin>>n;
+//     cout<<"Decimal of binary "<<n<<" is "<<convertBinarytoDecimal(n);
+//     return 0;
+// }
+
 #include <iostream>
 #include <cmath>
 using namespace std;
-int convertBinarytoDecimal(int n) {
-    int decimalNumber=0, i=0, dig;
+int convertBinarytoDecimal(int n)
+{
+    decimalnumber=0; i=0; dig;
     while(n!=0)
     {
         dig=n%10;
         n=n/10;
-        decimalNumber+=dig*pow(2,i);
+        decimalnumber+=dig*pow(2,i);
         ++i;
+
     }
-
-    return decimalNumber;
-
+    return decimalnumber;
 }
 
 int main()
