a3052c1588b34f244cde323918547680c215fab9
ayushxpal <ayush.pal23@lpu.in>
2024-08-11 18:10:35 +0530

    calculator

--- 11aug.cpp
+++ 11aug.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
-#include <string>
 using namespace std;
 int main()
 {
-    string firstname="Ayush";
-    cout<<"length of firstname is "<<firstname.length()<<endl;
-    return 0;
+    char operation;
+    float num1, num2;
+    cout << "Enter the operation you want to perform: ";
+    cin>>operation;
+    cout<<"Enter two numbers: ";
+    cin>>num1>>num2;
 
+    switch(operation)
+    {
+        case '+':
+        cout<<num1<<" + "<<num2<<" = "<<num1+num2;
+        case '-':
+        cout<<num1<<" - "<<num2<<" = "<<num1-num2;
+        case '*':
+        cout<<num1<<" * "<<num2<<" = "<<num1*num2;
+        case '/':
+        if(num2!=0)
+        {
+            cout<<num1<<" / "<<num2<<" = "<<num1/num2;
+        }
+        else
+        {
+            cout<<"Division by zero is not possible";
+        }
+        break;
+        default:
+        cout<<"Invalid operation";
+        break;  
+        return 0;
+    }
 }
