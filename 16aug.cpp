00258446deb64bab3c1f7e8ee33418e8f757815d
ayushxpal <ayush.pal23@lpu.in>
2024-08-16 22:54:14 +0530

    insertion at the end

--- 16aug.cpp
+++ 16aug.cpp
@@ -2,10 +2,22 @@
 using namespace std;
 int main()
 {
-    int arr[]={1,22,33,44,5};
+    int arr[10]={1,2,3,4,5};
+    int newElement=9;
 
-    for(int i=0;i<5;i++)
+    int size=0;
+    while(size<10 && arr[size]!=0)
     {
-        cout<<arr[i]<<endl;
+        size++;
     }
+
+    arr[size]=newElement;
+
+    for(int i=0;i<=size;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+    
 }
