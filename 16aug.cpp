2aa9971dcbcc0504c5f38d76ad607bb245289baa
ayushxpal <ayush.pal23@lpu.in>
2024-08-16 17:19:45 +0530

    traversal of an array

--- 16aug.cpp
+++ 16aug.cpp
@@ -1,43 +1,11 @@
 #include <iostream>
 using namespace std;
+int main()
+{
+    int arr[]={1,22,33,44,5};
 
-void printLine(char color) {
-    for (int i = 0; i < 30; i++) {
-        cout << color;
+    for(int i=0;i<5;i++)
+    {
+        cout<<arr[i]<<endl;
     }
-    cout << endl;
-}
-
-void printAshokaChakra() {
-    for (int i = 0; i < 30; i++) {
-        if (i == 14 || i == 15) {
-            cout << "O";  // Representing Ashoka Chakra
-        } else {
-            cout << " ";
-        }
-    }
-    cout << endl;
-}
-
-int main() {
-    // Top band (Saffron color)
-    for (int i = 0; i < 4; i++) {
-        printLine('#');  // '#' represents saffron color
-    }
-
-    // Middle band (White color with Ashoka Chakra)
-    for (int i = 0; i < 2; i++) {
-        printLine(' ');  // ' ' represents white color
-    }
-    printAshokaChakra();
-    for (int i = 0; i < 2; i++) {
-        printLine(' ');  // ' ' represents white color
-    }
-
-    // Bottom band (Green color)
-    for (int i = 0; i < 4; i++) {
-        printLine('+');  // '+' represents green color
-    }
-
-    return 0;
 }
