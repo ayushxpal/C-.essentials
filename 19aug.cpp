8f4337b6d29ee2b0b5bf2a00f299b452c8edb4c4
ayushxpal <ayush.pal23@lpu.in>
2024-08-19 23:52:13 +0530

    getter in OOP

---
+++ 19aug.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class student
+{
+    string name;
+    int age;
+    int id;
+    string grade;
+
+    
+};
+
+int main()
+{
+    student s1;
+    cin>>s1.name;
+    cout<<s1.name;
+}
