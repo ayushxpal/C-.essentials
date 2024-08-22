031d10ec2145a5e11a6dc61bb1335ae7b5c13878
ayushxpal <ayush.pal23@lpu.in>
2024-08-22 16:42:45 +0530

    delete anywhere

--- 22aug.cpp
+++ 22aug.cpp
@@ -2,21 +2,30 @@
 using namespace std;
 int main()
 {
-    int array[10], n;
+    int array[10],n,y;
     cin>>n;
-    for(int i=0; i<n; i++)
+    for(int i=0;i<n;i++)
     {
         cin>>array[i];
     }
-    
-
-    for(int i=0; i<n-1; i++)
+    cin>>y;
+    if(y>=n+1)
     {
-        array[i]= array[i+1];
+        cout<<"invalid position"<<endl;
     }
+
+    else{
+
+        for(int i=y-1;i<n-1;i++)
+        {
+            array[i]=array[i+1];
+        }
+    }
+
     n--;
-    for(int i=0; i<n; i++)
+    for(int i=0;i<n;i++)
     {
         cout<<array[i];
     }
+    return 0;
 }
