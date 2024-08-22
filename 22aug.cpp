6932409e6ca4be372e80ef1f414a2c98daebb064
ayushxpal <ayush.pal23@lpu.in>
2024-08-22 10:54:25 +0530

    22aug class

---
+++ 22aug.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1=70;
+    double num2=264.112;
+    char ch='A';
+
+    cout<<num1<<endl;
+    cout<<num2<<endl;
+    cout<<ch<<endl;
+
+    return 0;
+}
