ac3d5a0e06a839ad109558c9444c203f7e10fd68
ayushxpal <ayush.pal23@lpu.in>
2024-08-24 15:02:28 +0530

    neo colab 2

--- neocolab.cpp
+++ neocolab.cpp
@@ -1,59 +1,69 @@
-// Single File Programming Question
 // Problem Statement
 
 
 
-// Banu wants to find the number of elements less than or equal to a given key in a sorted array. 
+// Emma has a collection of unique plant species in her garden. She wants to check if a particular plant is already in her collection. 
 
 
 
-// Help her write a program that takes the array, the size of the array, and the key as input and returns the count of such elements, using the binary search technique.
-
-
-
-// Company Tags: Accenture
+// Help Emma by writing a program using a linear search algorithm that determines if the plant species is in her collection.
 
 // Input format :
-// The first line of input consists of the integer n, the size of the array.
+// The first line of input consists of an integer n, representing the number of plant species.
 
-// The second line consists of n space-separated integers, representing the elements of the sorted array.
+// The second line consists of n space-separated integers, representing the plant species IDs in Emma's collection.
 
-// The third line consists of an integer k.
+// The third line consists of an integer representing the target plant species ID.
 
 // Output format :
-// The output prints the count of elements in the array that are less than or equal to the given value k.
+// The output prints whether the target plant species ID is present in the collection or not.
+
+
+
+// Refer to the sample output for the exact format.
 
 // Code constraints :
-// 1 ≤ n ≤ 10
+// 1 ≤ n ≤ 25
 
-// -100 ≤ elements of the array ≤ 100
+// 1 ≤ species ID, target ≤ 100
+
 
 
 #include <iostream>
 using namespace std;
 int main()
 {
-    int n, key;
-    cin>>n;
-    int arr[n];
-    for(int i=0; i<n; i++)
-    {
-        cin>>arr[i];
-    }
-    
-    cin>>key;
-    
-    int count=0;
-    for(int i=0; i<n; i++)
-    {
-        if(arr[i]<=key)
+        int n;
+        cin>>n;
+        int species[25];
+        
+        for (int i=0; i<n; ++i)
         {
-            count++;
-            
+            cin>>species[i];
         }
-    }
+        
+        int target;
+        cin>>target;
+        bool found=false;
+        for(int i=0; i<n; ++i)
+        {
+            if(species[i] == target)
+            {
+                found=true;
+                break;
+            }
+        }
+        
+        if(found)
+        {
+            cout<<"Plant species "<<target<<" is present in the collection"<<endl;
+        }
+        else
+        {
+            cout<<"Plant species "<<target<<" is not present in the collection"<<endl;
+        }
+        
+        return 0;
+        
     
-    cout<<count<<endl;
-    
-    return 0;
 }
