ece700ddaf9d3d759cc888dba143c93544a8cad8
ayushxpal <ayush.pal23@lpu.in>
2024-08-27 15:15:43 +0530

    neo colab

--- neocolab.cpp
+++ neocolab.cpp
@@ -1,22 +1,21 @@
+// Single File Programming Question
 // Problem Statement
 
 
 
-// You're an instructor managing a list of exam scores for a group of students. The scores are stored in an array, and you need to sort this array in ascending order so that you can easily identify the highest and lowest scores. You'll use two different sorting algorithms: bubble sort and selection sort.
+// You're a coach managing a list of finishing times for athletes in a race. The times are stored in an array, and you need to sort this array in ascending order to determine the rankings.
 
 
 
-// Bubble sort is selected if the array is nearly sorted. An array is considered nearly sorted if at most one pair of elements is out of order. The selection sort is chosen if the array is not nearly sorted.
+// You'll use the insertion sort algorithm to accomplish this.
 
 // Input format :
-// The first line of input contains an integer n, representing the number of students.
+// The first line of input contains an integer n, representing the number of athletes.
 
-// The second line contains n space-separated integers, representing the exam scores of students.
+// The second line contains n space-separated integers, each representing the finishing time of an athlete in seconds.
 
 // Output format :
-// The first line of output prints which sort algorithm is used, either "Using Bubble Sort" or "Using Selection Sort".
-
-// The second line prints the sorted exam scores in ascending order.
+// The output prints the sorted finishing times of the athletes in ascending order.
 
 
 
@@ -27,92 +26,72 @@
 
 // 1 ≤ n ≤ 20
 
-// 1 ≤ exam score ≤ 100
+// 10 ≤ finishing time ≤ 120
 
 // Sample test cases :
 // Input 1 :
-// 6
-// 64 78 85 92 99 73
+// 5
+// 75 89 65 90 70
 // Output 1 :
-// Using Bubble Sort
-// Sorted Exam Scores: 64 73 78 85 92 99 
+// 65 70 75 89 90 
 // Input 2 :
 // 5
-// 32 91 53 76 28
+// 60 65 70 75 80
 // Output 2 :
-// Using Selection Sort
-// Sorted Exam Scores: 28 32 53 76 91 
+// 60 65 70 75 80 
+// Input 3 :
+// 5
+// 90 85 80 75 70
+// Output 3 :
+// 70 75 80 85 90 
+// Input 4 :
+// 5
+// 80 85 80 70 85
+// Output 4 :
+// 70 80 80 85 85 
 // Note :
 // The program will be evaluated only after the “Submit Code” is clicked.
 // Extra spaces and new line characters in the program output will result in the failure of the test case.
+
 
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-// Function to determine if the array is nearly sorted
-bool isNearlySorted(const vector<int>& scores) {
-    int outOfOrder = 0;
-    for (int i = 0; i < scores.size() - 1; ++i) {
-        if (scores[i] > scores[i + 1]) {
-            outOfOrder++;
-            if (outOfOrder > 1) {
-                return false; // More than one pair out of order
-            }
-        }
-    }
-    return true; // Zero or one pair out of order
-}
+// Function to perform insertion sort
+void insertionSort(vector<int>& times) {
+    int n = times.size();
+    for (int i = 1; i < n; ++i) {
+        int key = times[i];
+        int j = i - 1;
 
-// Bubble sort function
-void bubbleSort(vector<int>& scores) {
-    int n = scores.size();
-    for (int i = 0; i < n - 1; ++i) {
-        for (int j = 0; j < n - i - 1; ++j) {
-            if (scores[j] > scores[j + 1]) {
-                swap(scores[j], scores[j + 1]);
-            }
+        // Move elements of times[0..i-1], that are greater than key, to one position ahead of their current position
+        while (j >= 0 && times[j] > key) {
+            times[j + 1] = times[j];
+            j--;
         }
-    }
-}
-
-// Selection sort function
-void selectionSort(vector<int>& scores) {
-    int n = scores.size();
-    for (int i = 0; i < n - 1; ++i) {
-        int minIndex = i;
-        for (int j = i + 1; j < n; ++j) {
-            if (scores[j] < scores[minIndex]) {
-                minIndex = j;
-            }
-        }
-        swap(scores[i], scores[minIndex]);
+        times[j + 1] = key;
     }
 }
 
 int main() {
     int n;
     cin >> n;
-    vector<int> scores(n);
     
+    vector<int> times(n);
     for (int i = 0; i < n; ++i) {
-        cin >> scores[i];
-    }
-
-    if (isNearlySorted(scores)) {
-        cout << "Using Bubble Sort" << endl;
-        bubbleSort(scores);
-    } else {
-        cout << "Using Selection Sort" << endl;
-        selectionSort(scores);
+        cin >> times[i];
     }
     
-    cout << "Sorted Exam Scores:";
-    for (const int& score : scores) {
-        cout << " " << score;
+    // Sort times using insertion sort
+    insertionSort(times);
+    
+    // Print the sorted times
+    for (const int& time : times) {
+        cout << time << " ";
     }
     cout << endl;
     
     return 0;
-}
+}// You are using GCC
