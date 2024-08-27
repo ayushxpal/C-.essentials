364e25b5f9729d7011dcb63b29b3f8d7ac8580ed
ayushxpal <ayush.pal23@lpu.in>
2024-08-27 23:39:37 +0530

    neo colab2

--- NEOCOLABcpp.cpp
+++ NEOCOLABcpp.cpp
@@ -2,129 +2,132 @@
 
 
 
-// John is developing a simulation system for different types of vehicles to calculate their speeds under various conditions using function overloading. The system can compute the speed of a car, a train, and a plane based on the given distance, time, and additional acceleration values.
+// Seema is building a ticket reservation system and she wants to reserve tickets and cancel for a particular id. She created two classes: Ticket and Reservation. Each Ticket has a private attribute ticketId, and each Reservation has a private attribute reservedTickets (an array of tickets). The Reservation class contains a friend function cancelReservation(Reservation&, const Ticket&) that allows a ticket to be canceled from a reservation. 
 
 
 
-// Formula:
-
-// The speed of the car is calculated using the formula: speed = distance/time
-
-// The speed of the train or plane is calculated using the formula: speed = (distance/time) + acceleration*time
+// Implement the classes and the cancelReservation function and write a program to manage ticket reservations.
 
 
 
-// Example
-
-
-
-// Input:
-
-// 100.5
-
-// 10.0
-
-// 5.5
-
-// 6.6
-
-
-
-// Output:
-
-// 10.05
-
-// 65.05
-
-// 76.05
-
-
-
-// Function Specifications:
-
-// double calculateSpeed(double distance, double time)
-
-// double calculateSpeed(double distance, double time, double acceleration)
-
-
-
-// Note: This is a sample question asked in Capgemini recruitment.
+// Note: This is a sample question asked in Accenture recruitment.
 
 // Input format :
-// The first line of input consists of the distance(in meters) as a double value.
+// The first line consists of an integer N, representing the total number of tickets N to be reserved.
 
-// The second line of input consists of the time(in seconds) as a double value.
+// The second line consists of N space-separated integers, representing the ticket IDs.
 
-// The third line of input consists of the acceleration(in m/s2) of the train as a double value.
-
-// The third line of input consists of the acceleration(in m/s2) of the plane as a double value.
+// The third line consists of an integer, representing the ticket ID for which the reservation has to be cancelled.
 
 // Output format :
-// The first line of output prints the speed of the car(m/s) rounded off to two decimal places.
-
-// The second line of output prints the speed of the train(m/s) rounded off to two decimal places.
-
-// The third line of output prints the speed of the plane(m/s) rounded off to two decimal places.
+// The first line displays the reserved tickets in the format: "Reserved Tickets: <ticketId1> <ticketId2> ... <ticketIdN>"
 
 
 
-// Refer to the sample output for the formatting specifications.
+// The second line displays the cancellation status in the format:
+
+// If the ticket is found and cancelled: "Ticket with ID <ticketId> has been cancelled".
+// If the ticket is not found: "Ticket with ID <ticketId> not found in the reservation".
+
+
+// The third line displays the updated reserved tickets in the format: "Reserved Tickets: <ticketId1> <ticketId2> ... <ticketIdN>".
+
+
+
+// Refer to the sample outputs for the formatting specifications.
 
 // Code constraints :
-// 1.0 ≤ distance ≤ 10000.0
-
-// 1.0 ≤ time ≤ 100.0
-
-// 1.0 ≤ accelerations of train and plane ≤ 50.0
+// 1 ≤ N ≤ 25
 
 // Sample test cases :
 // Input 1 :
-// 100.5
-// 10.0
-// 5.5
-// 6.6
+// 5
+// 101 103 105 108 110
+// 110
 // Output 1 :
-// 10.05
-// 65.05
-// 76.05
+// Reserved Tickets: 101 103 105 108 110 
+// Ticket with ID 110 has been cancelled
+// Reserved Tickets: 101 103 105 108 
+// Input 2 :
+// 5
+// 101 103 105 108 110
+// 111
+// Output 2 :
+// Reserved Tickets: 101 103 105 108 110 
+// Ticket with ID 111 not found in the reservation
+// Reserved Tickets: 101 103 105 108 110 
 // Note :
 // The program will be evaluated only after the “Submit Code” is clicked.
 // Extra spaces and new line characters in the program output will result in the failure of the test case.
 
 
-// You are using GCC
 #include <iostream>
-#include <iomanip>
-
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-// Function to calculate speed of a car
-double calculateSpeed(double distance, double time) {
-    return distance / time;
-}
+class Ticket {
+private:
+    int ticketId;
+public:
+    Ticket(int id) : ticketId(id) {}
+    int getTicketId() const { return ticketId; }
+};
 
-// Function to calculate speed of a train or plane
-double calculateSpeed(double distance, double time, double acceleration) {
-    return (distance / time) + (acceleration * time);
+class Reservation {
+private:
+    vector<Ticket> reservedTickets;
+
+    void printReservedTickets() const {
+        cout << "Reserved Tickets:";
+        for (const Ticket &ticket : reservedTickets) {
+            cout << " " << ticket.getTicketId();
+        }
+        cout << endl;
+    }
+
+    friend void cancelReservation(Reservation&, int);
+
+public:
+    Reservation(vector<int> ticketIds) {
+        for (int id : ticketIds) {
+            reservedTickets.push_back(Ticket(id));
+        }
+    }
+
+    void displayStatusAndCancel(const int &ticketId) {
+        printReservedTickets();
+        cancelReservation(*this, ticketId);
+        printReservedTickets();
+    }
+};
+
+void cancelReservation(Reservation &res, int ticketID) {
+    auto it = find_if(res.reservedTickets.begin(), res.reservedTickets.end(),
+        [ticketID](const Ticket &ticket) {
+            return ticket.getTicketId() == ticketID;
+        });
+
+    if (it != res.reservedTickets.end()) {
+        cout << "Ticket with ID " << ticketID << " has been cancelled" << endl;
+        res.reservedTickets.erase(it);
+    } else {
+        cout << "Ticket with ID " << ticketID << " not found in the reservation" << endl;
+    }
 }
 
 int main() {
-    double distance, time, trainAcceleration, planeAcceleration;
+    int N;
+    cin >> N;
+    vector<int> ticketIds(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> ticketIds[i];
+    }
+    int cancelId;
+    cin >> cancelId;
 
-    // Input the values
-    cin >> distance >> time >> trainAcceleration >> planeAcceleration;
-
-    // Calculate and print the speed of the car
-    double carSpeed = calculateSpeed(distance, time);
-    cout << fixed << setprecision(2) << carSpeed << endl;
-
-    // Calculate and print the speed of the train
-    double trainSpeed = calculateSpeed(distance, time, trainAcceleration);
-    cout << fixed << setprecision(2) << trainSpeed << endl;
-
-    // Calculate and print the speed of the plane
-    double planeSpeed = calculateSpeed(distance, time, planeAcceleration);
-    cout << fixed << setprecision(2) << planeSpeed << endl;
+    Reservation reservation(ticketIds);
+    reservation.displayStatusAndCancel(cancelId);
 
     return 0;
 }
