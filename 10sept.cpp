16c4be095b3b3fc7c21f0e8effcf3001f01eba95
ayushxpal <ayush.pal23@lpu.in>
2024-09-10 17:12:58 +0530

    push

--- 10sept.cpp
+++ 10sept.cpp
@@ -1,36 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-struct Node {
-    int data;
-    Node *next;
-    Node *prev;
-};
-
-Node *head = NULL;
-void insert (int n) {
-    Node *newNode = new Node;
-    newNode->data = n;
-    newNode->next = head;
-    newNode -> prev = NULL;
-    if(head != NULL) {
-        head->prev=newNode;
-    }
-    head = newNode;
+inline int remainder(int a, int b)
+{
+    return a%b;
 }
-
-void print() {
-    cout << "Data element in singly linked list are: ";
-    Node *temp=head;
-    while(temp!=NULL) {
-        cout << temp->data<<" ";
-        temp=temp->next;
-    }
-}
-
-int main() {
-    insert(20);
-    insert(30);
-    print();
+int main()
+{
+    int a,b;
+    cout<<"enter two numbers: ";
+    cin>>a>>b;
+    cout<<"remainder: "<<remainder(a,b)<<endl;
     return 0;
 }
