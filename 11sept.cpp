9bcf370d5cae70fff7abea7d51bf9b58b17b08d4
ayushxpal <ayush.pal23@lpu.in>
2024-09-13 00:49:17 +0530

    push

--- 11sept.cpp
+++ 11sept.cpp
@@ -1,233 +1,61 @@
-// // // Single File Programming Question
-// // // Problem Statement
-
-
-
-// // // You are tasked with creating a simple calculator program that performs various arithmetic operations on two integers. The program should allow the user to input two integers and then calculate and display the results of addition, subtraction, multiplication, division, and modulus.
-
-
-
-// // // Requirements:
-
-
-
-// // // The program should prompt the user to enter two integers.
-// // // It should provide the following functionalities:
-// // // Calculate and display the sum of the two integers.
-// // // Calculate and display the difference between the two integers.
-// // // Calculate and display the product of the two integers.
-// // // Calculate and display the division of the first integer by the second, with a check to prevent division by zero.
-// // // Calculate and display the modulus of the first integer by the second, also ensuring that division by zero does not occur.
-// // // Input format :
-// // // The input consists of two integer values a and b.
-
-// // // Output format :
-// // // The output prints the result of arithmetic operations.
-
-
-
-// // // Refer to the sample outputs for formatting specifications.
-
-// // // Code constraints :
-// // // In this scenario, the test cases fall under the following constraints:
-
-// // // -1000 ≤ a, b ≤ 1000
-
-
 // // #include <iostream>
 // // using namespace std;
-// // class Calculator
-// // {
-// //     public:
-// //     int a,b;
-// //     void getinput()
-// //     {
-// //         cout<<"enter two numbers: ";
-// //         cin>>a>>b;
-
-// //     }
-
-// //     void getoutput()
-// //     {
-// //         cout<<"sum: "<<a+b<<endl;
-// //         cout<<"difference: "<<a-b<<endl;
-// //         cout<<"multilpication: "<<a*b<<endl;
-// //         if(b!=0)
-// //         {
-// //             cout<<"division: "<<a/b<<endl;
-// //             cout<<"modulus: "<<a%b<<endl;
-            
-// //         }
-// //         else
-// //         {
-// //             cout<<"Division by zero not possible"<<endl;
-// //             cout<<"Division by zero not possible"<<endl;
-// //         }
-// //     }
-// // };
-
 // // int main()
 // // {
-// //     Calculator calc;
-// //     calc.getinput();
-// //     calc.getoutput();
+// //     int a,b,temp;
+// //     cin>>a>>b;
+// //     temp=a;
+// //     a=b;
+// //     b=temp;
+// //     cout<<a<<" "<<b;
 // //     return 0;
 // // }
 
 
-// // Single File Programming Question
-// // Problem Statement
-
-
-
-// // Zara is developing a navigation system for a robot. The system uses directions to determine the robot's movement. So, implement a Navigator class that can receive direction input and provide corresponding navigation instructions.
-// // A weather forecasting application needs a program that can convert temperature values between Celsius, Fahrenheit, and Kelvin. The program should allow the user to input a temperature in Celsius and display its equivalent values in Fahrenheit and Kelvin.
-
-
-
-// // In this scenario, the program allows you to input a direction using integer values representing the cardinal directions (North, East, West, and South). The code then utilizes the Navigator class to determine the corresponding navigation instruction based on the input direction.
-// // You are tasked with creating a temperature conversion program using a class called TemperatureConverter. The program should allow users to input a temperature in Celsius and display its equivalent values in Fahrenheit and Kelvin. Use a non-inline member function to calculate the converted temperatures.
-
-
-
-// // Create an enum with all four directions: North, East, West, and South as the constants.
-// // Formulas:
-
-// // Celsius to Fahrenheit	= 9/5 (Celcius) + 32
-// // Celsius to Kelvin	K = Celcius + 273.15
-
-
-// // We will then use the switch case statements to switch between the direction elements and print the output based on the value of the variable for the enum directions. Write a program to print the direction it is heading towards.
-// // Company Tags: Cocubes 
-
-// // Input format :
-// // The input consists of an integer representing the direction.
-// // The input consists of a single line containing a double value representing the temperature in Celsius.
-
-// // Output format :
-// // The output prints whether the direction is North (or) South (or) East (or) West (or) Invalid Input.
-// // The first line of the output displays the temperature in Fahrenheit, as a double value rounded to two decimal places.
-
-// // The second line of the output displays the temperature in Kelvin, as a double value rounded to two decimal places.
-
-
-
-// // Refer to the sample output for formatting specifications.
-
-// // Code constraints :
-// // 1 represents North.
-
-// // 2 represents East.
-
-// // 3 represents West.
-// // The temperature range is not limited.
-
-// // 4 represents South.
-// // The program should handle positive and negative temperature values.
-
-// // Sample test cases :
-// // Input 1 :
-// // 1
-// // 25.5
-// // Output 1 :
-// // We are headed towards North.
-// // Temperature in Fahrenheit: 77.90
-// // Temperature in Kelvin: 298.65
-// // Input 2 :
-// // 12
-// // 0
-// // Output 2 :
-// // Invalid Input
-// // Temperature in Fahrenheit: 32.00
-// // Temperature in Kelvin: 273.15
-// // Input 3 :
-// // -22
-// // Output 3 :
-// // Temperature in Fahrenheit: -7.60
-// // Temperature in Kelvin: 251.15
-// // Note :
-// // The program will be evaluated only after the “Submit Code” is clicked.
-// // Extra spaces and new line characters in the program output will result in the failure of the test case.
-
 // #include <iostream>
-// #include <iomanip>
 // using namespace std;
-// enum Directions
-// {
-//     North = 1,
-//     East = 2,
-//     West = 3,
-//     South = 4
-// };
-
-
-
-// void printdirection(int direction)
-// {
-//     switch(direction)
-//     {
-//         case North:
-//         cout<<"We are headed towards North."<<endl;
-//         break;
-
-//         case East:
-//         cout<<"We are headed towards East."<<endl;
-//         break;
-
-//         case West:
-//         cout<<"We are headed towards West."<<endl;
-//         break;
-
-//         case South:
-//         cout<<"We are headed towards South."<<endl;
-//         break;
-
-//         default:
-//         cout<<"Invalid Input"<<endl;
-//         break;
-//     }
-// }
-
-// void converttemp(double celsius)
-// {
-//     double fahrenheit = (9/5)*celsius + 32;
-//     double kelvin = celsius + 273.15;
-
-//     cout<<fixed<<setprecision(2);
-//     cout<<"Temperature in Fahrenheit: "<<fahrenheit<<endl;
-//     cout<<"Temperature in Kelvin: "<<kelvin<<endl;
-
-// }
-
-
-
 // int main()
 // {
-//     int direction;
-//     double celsius;
-//     cin>>direction;
-//     cin>>celsius;
-//     printdirection(direction);
-//     converttemp(celsius);
+//     char ch;
+//     cout<<"Enter a character: ";
+//     cin>>ch;
+//     if( (ch>='a' && ch<='z') || (ch>='A') && ch<='Z')
+//     {
+//         cout<<ch<<" is an alphabet";
+
+//     }
+//     else if(ch>='0'&&ch<='9')
+//     {
+//         cout<<ch<<" is a digit";
+
+//     }
+//     else
+//     {
+//         cout<<ch<<" is a special character";
+//     }
 //     return 0;
 // }
 
 
-#include <string>
+
 #include <iostream>
 using namespace std;
-main()
+int main()
 {
-    string s1("man");
-    string s2="hi";
-    string s3;
-    s3=s1;
-    cout<<"s3="<<s3<<endl;
+    char ch;
+    cout<<"enter a character: ";
+    cin>>ch;
+    if((ch>='a' && ch<='z') || (ch>='A') && ch<='Z')
+    {
+        cout<<ch<<" is a character";
 
-    s3="neither"+s1+"nor";
-    s3+=s2;
-    cout<<"s3="<<s3<<endl;
-    s1.swap(s2);
-    cout<<s1<<"nor"<<s2<<endl;
-    return 0;   
+    }
+    else if(ch>='0' && ch<='9')
+    {
+        cout<<ch<<" is a digit";
+    }
+    else
+    {
+        cout<<ch<<" is a special character";
+    }
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,83 +1,11 @@
-Problem Statement
-
-
-
-// Matt is learning C++ and is interested in how inline functions work. He is practicing questions related to this concept. Help him by writing an inline function countEvenNumbers() that counts the number of even digits present in the square of a given number.
-
-
-
-// Example 1
-
-
-
-// Input:
-
-// 12
-
-
-
-// Output:
-
-// 2
-
-
-
-// Explanation:
-
-// Given number = 12. After squaring (12*12 = 144). The number of even digits present is 2.
-
-
-
-// Example 2
-
-
-
-// Input:
-
-// 3
-
-
-
-// Output:
-
-// 0
-
-
-
-// Explanation:
-
-// Given number = 3. After squaring (3*3 = 9). The number of even digits present is 0.
-
-
-
-// Note: This is a sample question that can be asked in a mPhasis recruitment.
-
-// Input format :
-// The input consists of a positive integer N.
-
-// Output format :
-// The output prints an integer representing the count of even digits in the square of the given number.
-
-
-
-// Refer to the sample output for the formatting specifications.
-
-// Code constraints :
-// 0 ≤ N ≤ 1000
-
-// Sample test cases :
-// Input 1 :
-// 12
-// Output 1 :
-// 2
-// Input 2 :
-// 3
-// Output 2 :
-// 0
-// Input 3 :
-// 10
-// Output 3 :
-// 2
-// Note :
-// The program will be evaluated only after the “Submit Code” is clicked.
-// Extra spaces and new line characters in the program output will result in the failure of the test case.
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s1="bcme";
+    string s2="abcrome";
+    cout<<s1.compare(s2)<<endl;
+    cout<<s1.compare(2,2,s2,5,2);
+    return 0;
+}
