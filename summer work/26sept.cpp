48fc5157591f4300367847b5af89d48eed076843
ayushxpal <ayush.pal23@lpu.in>
2024-09-26 10:46:07 +0530

    push

--- summer work/26sept.cpp
+++ summer work/26sept.cpp
@@ -1,23 +1,53 @@
+// #include <iostream>
+// using namespace std;
+// class rectangle
+// {
+//     int l, b;
+//     public:
+//         rectangle(int x=12, int y=34)
+//         {
+//             l=x;
+//             b=y;
+//         }
+//         int area()
+//         {
+//             return l*b;
+//         }
+// };
+// int main()
+// {
+//     rectangle r;
+//     cout<<"Area is: "<<r.area()<<endl;
+//     rectangle r1(45,67);
+//     cout<<"Area is: "<<r1.area()<<endl;
+// } 
+
 #include <iostream>
 using namespace std;
-class rectangle
+class operator
 {
-    int l, b;
+    int c;
     public:
-        rectangle(int x=12, int y=34)
-        {
-            l=x;
-            b=y;
-        }
-        int area()
-        {
-            return l*b;
-        }
+    counter(int a)
+    {
+        c=a;
+    }
+    counter(counter &ob)
+    {
+        cout<<"copy constructor invoked";
+        c=ob.c;
+    }
+
+    void show()
+    {
+        cout<<c;
+    }
 };
+
 int main()
 {
-    rectangle r;
-    cout<<"Area is: "<<r.area()<<endl;
-    rectangle r1(45,67);
-    cout<<"Area is: "<<r1.area()<<endl;
-} 
+    counter c1(10);
+    counter c2(c1);
+    c1.show();
+    c2.show();
+}
