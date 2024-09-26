4891e41dadce65286ad539d355b903a13c12f2dc
ayushxpal <ayush.pal23@lpu.in>
2024-09-26 11:19:43 +0530

    26spet

--- summer work/26sept.cpp
+++ summer work/26sept.cpp
@@ -1,3 +1,57 @@
+// // // #include <iostream>
+// // // using namespace std;
+// // // class rectangle
+// // // {
+// // //     int l, b;
+// // //     public:
+// // //         rectangle(int x=12, int y=34)
+// // //         {
+// // //             l=x;
+// // //             b=y;
+// // //         }
+// // //         int area()
+// // //         {
+// // //             return l*b;
+// // //         }
+// // // };
+// // // int main()
+// // // {
+// // //     rectangle r;
+// // //     cout<<"Area is: "<<r.area()<<endl;
+// // //     rectangle r1(45,67);
+// // //     cout<<"Area is: "<<r1.area()<<endl;
+// // // } 
+
+// // #include <iostream>
+// // using namespace std;
+// // class operator
+// // {
+// //     int c;
+// //     public:
+// //     counter(int a)
+// //     {
+// //         c=a;
+// //     }
+// //     counter(counter &ob)
+// //     {
+// //         cout<<"copy constructor invoked";
+// //         c=ob.c;
+// //     }
+
+// //     void show()
+// //     {
+// //         cout<<c;
+// //     }
+// // };
+
+// // int main()
+// // {
+// //     counter c1(10);
+// //     counter c2(c1);
+// //     c1.show();
+// //     c2.show();
+// // }
+
 // #include <iostream>
 // using namespace std;
 // class rectangle
@@ -14,40 +68,31 @@
 //             return l*b;
 //         }
 // };
-// int main()
-// {
-//     rectangle r;
-//     cout<<"Area is: "<<r.area()<<endl;
-//     rectangle r1(45,67);
-//     cout<<"Area is: "<<r1.area()<<endl;
-// } 
 
 #include <iostream>
+#include <conio.h>
 using namespace std;
-class operator
+class counter
 {
-    int c;
+    int id;
     public:
-    counter(int a)
-    {
-        c=a;
-    }
-    counter(counter &ob)
-    {
-        cout<<"copy constructor invoked";
-        c=ob.c;
-    }
-
-    void show()
-    {
-        cout<<c;
-    }
+        counter()
+        {
+            id=i;
+            cout<<"constructor of object with id="<<id;
+        }
+        ~counter()
+        {
+            cout<<"destructor of object with id="<<id;
+        }
 };
 
 int main()
 {
-    counter c1(10);
-    counter c2(c1);
-    c1.show();
-    c2.show();
+    counter c1(1);
+    counter c2(2);
+    counter c3(3);
+    cout<<"\n end of main";
+
 }
+        
