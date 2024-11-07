d671fd8242f06d8cd275d1a1a18f47ecf51a3a0a
ayushxpal <ayush.pal23@lpu.in>
2024-11-07 11:19:46 +0530

    ayush

--- 7nov.cpp
+++ 7nov.cpp
@@ -1,27 +1,73 @@
+// #include <iostream>
+// using namespace std;
+// class BC
+// {
+//     public:
+//         void printBC()
+//         {
+//             cout<<"\nPrinting message in base class"<<endl;
+//         }
+//         void show()
+//         {
+//             cout<<"\nshow() of base class"<<endl;
+//         }
+// };
+
+// class DC: public BC{
+//     public:
+//     void printDC()
+//     {
+//         cout<<"\nPrinting message in derived class"<<endl;
+//     }
+//     void show()
+//     {
+//         cout<<"\nshow() of derived class"<<endl;
+//     }
+// };
+
+// int main()
+// {
+//     BC *bptr;
+//     BC base;
+//     bptr=&base;
+//     cout<<"bptr points to base objects\n";
+//     bptr->show();
+//     DC derived;
+//     bptr=&derived;
+//     cout<<"bptr now points to derived objects\n";
+//     bptr->show();
+//     DC *dptr;
+//     dptr=&derived;
+//     cout<<"dptr is derived type pointer\n";
+//     dptr->show();
+//     dptr->printDC();
+//     cout<<"using ((DC*)bptr)\n";
+//     ((DC*)bptr)->show();
+//     ((DC*)bptr)->printDC();
+//     return 0;
+// }
+
+
 #include <iostream>
 using namespace std;
 class BC
 {
     public:
-        void printBC()
-        {
-            cout<<"\nPrinting message in base class"<<endl;
-        }
-        void show()
+        virtual voidshow()
         {
             cout<<"\nshow() of base class"<<endl;
         }
 };
-
-class DC: public BC{
+class DC:public BC
+{
     public:
-    void printDC()
-    {
-        cout<<"\nPrinting message in derived class"<<endl;
-    }
     void show()
     {
-        cout<<"\nshow() of derived class"<<endl;
+        public:
+        void show()
+        {
+            cout<<"\nshow() of derived class"<<endl;
+        }
     }
 };
 
@@ -30,19 +76,12 @@ int main()
     BC *bptr;
     BC base;
     bptr=&base;
+    cout<<"\n-----Runtime polymorphism-----\n";
     cout<<"bptr points to base objects\n";
     bptr->show();
     DC derived;
     bptr=&derived;
     cout<<"bptr now points to derived objects\n";
     bptr->show();
-    DC *dptr;
-    dptr=&derived;
-    cout<<"dptr is derived type pointer\n";
-    dptr->show();
-    dptr->printDC();
-    cout<<"using ((DC*)bptr)\n";
-    ((DC*)bptr)->show();
-    ((DC*)bptr)->printDC();
     return 0;
 }
